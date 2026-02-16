875b938dd53a26989766b11472975ee2cf73f89e
enav330 <enav.gefen@post.runi.ac.il>
2026-02-16 20:47:11 +0200

    final

--- List.java
+++ List.java
@@ -24,26 +24,59 @@ public class List {
 
     /** Returns the CharData of the first element in this list. */
     public CharData getFirst() {
-        // Your code goes here
+        if (first == null) {
+            return null;
+        }
         return first.cp;
     }
 
     /** GIVE Adds a CharData object with the given character to the beginning of this list. */
     public void addFirst(char chr) {
-        // Your code goes here
+        CharData newCharData = new CharData(chr);
+        // Create a new node that points to the current first, and update first to be this new node
+        Node newNode = new Node(newCharData, first);
+        first = newNode;
+        size++;
     }
     
     /** GIVE Textual representation of this list. */
     public String toString() {
-        // Your code goes here
-        return "";
+    // אם הרשימה ריקה, מחזירים סוגריים ריקים
+    if (size == 0) {
+        return "()";
     }
 
+    StringBuilder str = new StringBuilder("(");
+    Node current = first;
+    
+    while (current != null) {
+        // מוסיפים את האיבר הנוכחי + רווח
+        str.append(current.cp.toString() + " ");
+        current = current.next; 
+    }
+
+    // כרגע המחרוזת נראית ככה: "(A B C " (עם רווח מיותר בסוף)
+    // הפקודה הבאה מורידה את הרווח האחרון
+    str.deleteCharAt(str.length() - 1);
+    
+    // ומוסיפה סוגריים סוגרים
+    str.append(")");
+
+    return str.toString();
+}
     /** Returns the index of the first CharData object in this list
      *  that has the same chr value as the given char,
      *  or -1 if there is no such object in this list. */
     public int indexOf(char chr) {
-        // Your code goes here
+        Node current = first;
+        int index = 0;
+        while (current != null) {
+            if (current.cp.chr == chr) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
         return -1;
     }
 
@@ -51,14 +84,40 @@ public class List {
      *  increments its counter. Otherwise, adds a new CharData object with the
      *  given chr to the beginning of this list. */
     public void update(char chr) {
-        // Your code goes here
+        Node current = first;
+        while (current != null) {
+            if (current.cp.chr == chr) {
+                current.cp.count++;
+                return;
+            }
+            current = current.next;
+        }
+        // If we reached here, the char was not found
+        addFirst(chr);
     }
 
     /** GIVE If the given character exists in one of the CharData objects
      *  in this list, removes this CharData object from the list and returns
      *  true. Otherwise, returns false. */
     public boolean remove(char chr) {
-        // Your code goes here
+        Node prev = null;
+        Node current = first;
+
+        while (current != null) {
+            if (current.cp.chr == chr) {
+                // If it's the first node
+                if (prev == null) {
+                    first = first.next;
+                } else {
+                    // Bypass the current node
+                    prev.next = current.next;
+                }
+                size--;
+                return true;
+            }
+            prev = current;
+            current = current.next;
+        }
         return false;
     }
 
@@ -66,10 +125,15 @@ public class List {
      *  If the index is negative or is greater than the size of this list, 
      *  throws an IndexOutOfBoundsException. */
     public CharData get(int index) {
-        // Your code goes here
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = first;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.cp;
     }
-
     /** Returns an array of CharData objects, containing all the CharData objects in this list. */
     public CharData[] toArray() {
 	    CharData[] arr = new CharData[size];
